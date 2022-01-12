857537313eb943829b4951c6ce6de598ab16ff87
Manmeet Gandhi <manmeetgandhi31@gmail.com>
2022-01-12 23:39:23 +0530

    code update

--- code.py
+++ code.py
@@ -9,7 +9,7 @@ header = {
     "Content-Type": "application/json",
     "Accept": "*/*",
     "Connection": "keep-alive",
-    "Authorization": "token <$your-personal-access-token>"
+    "Authorization": "token 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"
 }
 
 data = []
@@ -38,53 +38,71 @@ try:
             
 except getopt.error as err:
     print (str(err))
-    
-#check if organization exists
-orgExists = requests.get(
-    f'https://api.github.com/orgs/{orgName}',
-    headers=header)
-    
-if(orgExists.status_code == 200):
-	
-	#gets data for all repositories present for the given org
-	allRepos = requests.get(
-		f'https://api.github.com/orgs/{orgName}/repos',
-		headers=header).json()
-	
-	#calculate ic ratio for each repo and save corresponding data
-	for repo in allRepos:
-		open_issues = repo["open_issues_count"]
-		contributors = len(
-		requests.get(
-			repo["contributors_url"],
-			headers=header).json())
-		ic = float("{0:.3f}".format(open_issues / contributors))
-		repoData = []
-		repoData.append(repo["name"])
-		repoData.append(open_issues)
-		repoData.append(contributors)
-		repoData.append(ic)
-		data.append(repoData)
-	
-	#sort based on the ic ratio
-	sorteddata = sorted(data, key=lambda k: k[3], reverse=True)
-	final = sorteddata[:5]
-	
-	if(writeCSV):
-		with open("report.csv", "a", newline="") as f:
-			writer = csv.writer(f)
-			writer.writerow([datetime.now()])
-			writer.writerow(titles)
-			writer.writerows(final)
-		print('Successfully written to CSV file')
-	#if csv option not used as arguement directly display the result
+
+try:
+	#check if organization exists
+	orgExists = requests.get(
+		f'https://api.github.com/orgs/{orgName}',
+		headers=header)
+		
+	if(orgExists.status_code == 200):
+		
+		#gets data for all repositories present for the given org
+		allRepos = requests.get(f'https://api.github.com/orgs/{orgName}/repos', headers=header)
+		
+		allRepositories = []
+		if(allRepos.status_code == 200):
+			allRepositories = allRepos.json()
+		else:
+			print('Some error occurred, could not fetch repositories for given org - {orgName}')
+			sys.exit()
+		
+		#calculate ic ratio for each repo and save corresponding data
+		for repo in allRepositories:
+			open_issues = repo["open_issues_count"]
+			contributors = requests.get(repo["contributors_url"], headers=header)
+			contributors_len = 0
+			if(contributors.status_code == 200):
+				contributors_len = len(contributors.json())
+			else:
+				print(f"Some error occurred, could not fetch contributors for repo - {repo['name']}")
+				
+			ic = 0
+			if(contributors_len > 0):
+				ic = float("{0:.3f}".format(open_issues / contributors_len))
+				
+			repoData = []
+			repoData.append(repo["name"])
+			repoData.append(open_issues)
+			repoData.append(contributors_len)
+			repoData.append(ic)
+			
+			data.append(repoData)
+		
+		#sort based on the ic ratio
+		sorteddata = sorted(data, key=lambda k: k[3], reverse=True)
+		final = sorteddata[:5]
+		
+		if(writeCSV):
+			with open("report.csv", "a", newline="") as f:
+				writer = csv.writer(f)
+				writer.writerow([orgName, datetime.now()])
+				writer.writerow(titles)
+				writer.writerows(final)
+			print('Successfully written to CSV file')
+		#if csv option not used as arguement directly display the result
+		else:
+			myTable = PrettyTable(titles)
+
+			for item in final:
+				myTable.add_row(item)
+
+			print(myTable)
 	else:
-		myTable = PrettyTable(titles)
+		print('Given Org does not exists')
+		
+except Exception as err:
+	print(str(err))
+	
 
-		for item in final:
-			myTable.add_row(item)
-
-		print(myTable)
-else:
-	print('Given Org does not exists')
 
